a2a79f5f784870603e8e421c74d163b3c52d19f9
kheyal <piyaskheyal@gmail.com>
2026-04-14 01:00:48 +0600

    root link issue solved. coping from panda robot stayed. now fixed

--- launch/custom_demo.launch.py
+++ launch/custom_demo.launch.py
@@ -89,7 +89,7 @@ def generate_launch_description():
         executable="static_transform_publisher",
         name="static_transform_publisher",
         output="log",
-        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "root"],
     )
 
     # Publish TF
